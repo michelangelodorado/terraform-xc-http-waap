734cfefe99311966d78f34a811b502c0dfc0e931
Mike <102953584+michelangelodorado@users.noreply.github.com>
2024-04-16 14:10:38 +0800

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "api_cert" {
+            type = string
+            default = "tls.crt"
+        }
+        
+        variable "api_key" {
+          type = string
+          default = "tls.key"
+        }
+        
+        variable "api_url" {
+            type = string
+            default = "https://f5-apac-ent.console.ves.volterra.io/api"
+        }
